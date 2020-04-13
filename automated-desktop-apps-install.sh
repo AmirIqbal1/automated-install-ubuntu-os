36f0232c49ab99e8af650fa7247386913fb6a01a
Amir <AmirIqbal1@users.noreply.github.com>
2020-04-13 11:46:44 +0100

    updated to include latest plex version
    Fixed a typo & included some comments.

--- automated-desktop-apps-install.sh
+++ automated-desktop-apps-install.sh
@@ -48,7 +48,7 @@ python3-setuptools" -t 10
 echo ""
 
 #updates system ready for packages download
-
+echo "getting system ready for packages to be installed"
 dpkg --configure -a
 apt update -y
 apt upgrade -y
@@ -60,6 +60,7 @@ apt install -f
 apt install mat bleachbit curl dconf editor deluge gufw git gimp gdebi gnome-tweak-tool geany gparted htop libreoffice-calc libreoffice-writer openvpn preload shutter synaptic telegram-desktop virtualbox vlc unrar wget zip dialog python3-pip python3-setuptools -y
 
 #install stable chrome
+echo "installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 apt install ./google-chrome-stable_current_amd64.deb
@@ -67,6 +68,7 @@ apt install ./google-chrome-stable_current_amd64.deb
 rm google-chrome*.deb
 
 #install fb reader
+echo "installing FB Reader"
 wget https://fbreader.org/files/desktop/fbreader_0.99.4-1_amd64.deb
 
 apt install ./fbreader_0.99.4-1_amd64.deb
@@ -74,7 +76,7 @@ apt install ./fbreader_0.99.4-1_amd64.deb
 rm fbreader*.deb
 
 #install nextcloud client
-
+echo "installing Nextcloud Desktop Client"
 add-apt-repository ppa:nextcloud-devs/client -y
 
 apt update
@@ -82,7 +84,8 @@ apt update
 apt install nextcloud-client -y
 
 #install plex
-wget https://downloads.plex.tv/plex-media-server-new/1.19.1.2630-72c16a276/debian/plexmediaserver_1.19.1.2630-72c16a276_amd64.deb
+echo "Installing Plex Media Server"
+wget https://downloads.plex.tv/plex-media-server-new/1.19.1.2645-ccb6eb67e/debian/plexmediaserver_1.19.1.2645-ccb6eb67e_amd64.deb
 
 dpkg -i plex*
 
@@ -91,6 +94,7 @@ service plexmediaserver restart
 rm plex*.deb
 
 #install unetbootin
+echo "installing UNetBootin"
 add-apt-repository ppa:gezakovacs/ppa -y
 
 apt update
@@ -98,6 +102,7 @@ apt update
 apt install unetbootin -y
 
 #install veracrypt
+echo "installing VeraCrypt"
 add-apt-repository ppa:unit193/encryption -y
 
 apt update
@@ -105,6 +110,7 @@ apt update
 apt install veracrypt -y
 
 #install webtorrent
+echo "installing WebTorrent Desktop" 
 wget https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.21.0/webtorrent-desktop_0.21.0_amd64.deb
 
 dpkg -i webtorrent*.deb
@@ -112,7 +118,8 @@ dpkg -i webtorrent*.deb
 rm webtorrent*.deb
 
 #install protonvpn
+echo "installing ProtonVPN through pip3 package manager"
 pip3 install protonvpn-cli
 
 echo ""
-echo "All done, you can check the above to see if any errors occured. A system reboot is recommended."
+echo "All done, you can check above to see if any errors occured. A system reboot is recommended."
