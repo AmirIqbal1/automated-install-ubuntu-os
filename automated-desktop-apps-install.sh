cf7ba8d192be3dfe9e40f13a30a3459ce42d3d27
Amir <AmirIqbal1@users.noreply.github.com>
2022-01-03 13:01:31 +0000

    code go brrr

--- automated-desktop-apps-install.sh
+++ automated-desktop-apps-install.sh
@@ -4,14 +4,13 @@
 ## get UID 
 uid=$(id -u)
  
-## Check for it
-[ $uid -ne 0 ] && { echo "Only root may run this. Please login as root."; exit 1; }
+[ $uid -ne 0 ] && { echo "Only a root user may run this. Please login as root."; exit 1; }
 
 echo ""
-read -p "This script will auto install all programs on Ubuntu OS. View the README file to see what gets installed." -t 6
+read -p "This script will auto install selected programs on Ubuntu OS. View the README file to see what gets installed." -t 6
 echo ""
 
-#updates system ready for packages download
+#updates system ready for script
 echo "getting system ready for packages to be installed"
 dpkg --configure -a &&
 apt update &&
@@ -20,18 +19,13 @@ apt install -f &&
 apt clean &&
 apt autoclean 
 
-#these packages install from universe and ubuntu normal repo's
-#the rest install from wget commands
-
 apt install mat2 bleachbit curl dconf-editor deluge filezilla firefox gufw git gimp gdebi gnome-tweak-tool geany gparted htop libreoffice-calc libreoffice-writer openvpn rkhunter snap snapd synaptic tilix virtualbox vlc unrar wget zip zsh dialog gnupg apt-transport-https -y
 
-#install veracrypt & y-ppa-manager via PPA
-echo "installing VeraCrypt & y-ppa-manager"
+echo "installing veracrypt & y-ppa-manager via PPA"
 add-apt-repository ppa:unit193/encryption -y &&
 add-apt-repository ppa:webupd8team/y-ppa-manager -y &&
 apt update && apt install veracrypt y-ppa-manager -y
 
-#install apps through snap
 echo "Installing snap packages"
 snap install plexmediaserver nordpass telegram-desktop 
 
@@ -41,23 +35,16 @@ snap start plexmediaserver
 #allows nordpass snap to connect to browsers
 snap connect nordpass:password-manager-service
 
-#install deju dup backup tool
-echo "Installing Deja Dup Backup"
-snap install deja-dup --classic
+echo "Installing Deja-Dup Backup and Visual Studio Tool"
+snap install deja-dup --classic && snap install code --classic
 
-#install visual studio code
-echo "Installing Visual Studio Code"
-snap install code --classic
-
-#install balena etcher
 echo "installing balena etcher"
 curl -1sLf \
    'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' \
    | sudo -E bash &&
    apt update && apt install balena-etcher-electron -y
 
-#grabs and installs both webtorrent and foliate
-echo "installing WebTorrent Desktop" 
+echo "installing WebTorrent & Foliate" 
 wget https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.24.0/webtorrent-desktop_0.24.0_amd64.deb https://github.com/johnfactotum/foliate/releases/download/2.6.3/com.github.johnfactotum.foliate_2.6.3_all.deb
 dpkg -i webtorrent*.deb com.github.johnfactotum.foliate*.deb
 rm webtorrent*.deb com.github.johnfactotum.foliate*.deb
@@ -74,7 +61,7 @@ apt update
 curl -s https://packagecloud.io/install/repositories/firstlookmedia/code/script.deb.sh | sudo bash &&
 apt update && apt install dangerzone -y
 
-#fixes any errors and auto cleans packages downloaded/ not needed anymore
+#fixes any errors and auto cleans packages
 echo "auto cleaning now, and fixing any errors."
 dpkg --configure -a &&
 apt install -f &&
