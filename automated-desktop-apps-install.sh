c13e241a45ac88324d7c9567ab23c4c603c83443
Amir <AmirIqbal1@users.noreply.github.com>
2022-01-03 12:52:27 +0000

    the overhaul no.2

--- automated-desktop-apps-install.sh
+++ automated-desktop-apps-install.sh
@@ -8,8 +8,7 @@ uid=$(id -u)
 [ $uid -ne 0 ] && { echo "Only root may run this. Please login as root."; exit 1; }
 
 echo ""
-read -p "This script will auto install all your programs. View the README file to see what gets installed." -t 6
-
+read -p "This script will auto install all programs on Ubuntu OS. View the README file to see what gets installed." -t 6
 echo ""
 
 #updates system ready for packages download
@@ -24,19 +23,19 @@ apt autoclean
 #these packages install from universe and ubuntu normal repo's
 #the rest install from wget commands
 
-apt install mat2 bleachbit curl dconf-editor deluge filezilla firefox gufw git gimp gdebi gnome-tweak-tool geany gparted htop libreoffice-calc libreoffice-writer openvpn rkhunter snap snapd synaptic tilix virtualbox vlc unrar wget zip zsh dialog python3-pip python3-setuptools gnupg apt-transport-https -y
+apt install mat2 bleachbit curl dconf-editor deluge filezilla firefox gufw git gimp gdebi gnome-tweak-tool geany gparted htop libreoffice-calc libreoffice-writer openvpn rkhunter snap snapd synaptic tilix virtualbox vlc unrar wget zip zsh dialog gnupg apt-transport-https -y
 
 #install veracrypt & y-ppa-manager via PPA
 echo "installing VeraCrypt & y-ppa-manager"
-add-apt-repository ppa:unit193/encryption -y
-add-apt-repository ppa:webupd8team/y-ppa-manager -y
+add-apt-repository ppa:unit193/encryption -y &&
+add-apt-repository ppa:webupd8team/y-ppa-manager -y &&
 apt update && apt install veracrypt y-ppa-manager -y
 
 #install apps through snap
 echo "Installing snap packages"
-snap install plexmediaserver foliate nordpass telegram-desktop 
+snap install plexmediaserver nordpass telegram-desktop 
 
-snap stop plexmediaserver
+snap stop plexmediaserver &&
 snap start plexmediaserver
 
 #allows nordpass snap to connect to browsers
@@ -52,31 +51,27 @@ snap install code --classic
 
 #install balena etcher
 echo "installing balena etcher"
-echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-apt update && apt install balena-etcher-electron -y
+curl -1sLf \
+   'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' \
+   | sudo -E bash &&
+   apt update && apt install balena-etcher-electron -y
 
-#install webtorrent
+#grabs and installs both webtorrent and foliate
 echo "installing WebTorrent Desktop" 
-wget https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.24.0/webtorrent-desktop_0.24.0_amd64.deb
-dpkg -i webtorrent*.deb
-rm webtorrent*.deb
+wget https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.24.0/webtorrent-desktop_0.24.0_amd64.deb https://github.com/johnfactotum/foliate/releases/download/2.6.3/com.github.johnfactotum.foliate_2.6.3_all.deb
+dpkg -i webtorrent*.deb com.github.johnfactotum.foliate*.deb
+rm webtorrent*.deb com.github.johnfactotum.foliate*.deb
 
-#automatically gets my other script's
+#auto gets my other script's
 echo "Grabbing other scripts: rkhunter-check, auto-update-ubuntu-script"
-
 wget https://raw.githubusercontent.com/AmirIqbal1/auto-update-ubuntu-script/master/update.sh https://raw.githubusercontent.com/AmirIqbal1/rkhunter-script/master/rkhunter-check.sh
-
-#automatically chmods above scripts
-echo "chmodding those scripts"
+echo "chmodding above scripts"
 chmod +x update.sh rkhunter-check.sh
 
 #install dangerzone
 echo "installing dangerzone"
 apt update
-curl -L https://packagecloud.io/firstlookmedia/code/gpgkey | sudo apt-key add -
-#change the below line if you have different OS
-echo "deb https://packagecloud.io/firstlookmedia/code/ubuntu/ bionic main" | sudo tee -a /etc/apt/sources.list.d/firstlookmedia_code.list
+curl -s https://packagecloud.io/install/repositories/firstlookmedia/code/script.deb.sh | sudo bash &&
 apt update && apt install dangerzone -y
 
 #fixes any errors and auto cleans packages downloaded/ not needed anymore
@@ -91,4 +86,4 @@ echo ""
 echo -e "\e[42mrkhunter has been installed, please configure it using link in README.md file."
 
 echo ""
-echo "All done! You can check above to see if any errors occured. A system reboot is recommended."
+echo "All done! You should check above to see if any errors occured. A system reboot is recommended."
